1c54601e8e944706552dd3441eef67ed4806ad86
Кисленко Илья <kislenko@sputnikfund.ru>
2025-07-13 20:46:57 +0700

    chore: config changes

--- bundler.config.js
+++ bundler.config.js
@@ -1,7 +1,12 @@
 module.exports = {
     entry: "./src/index.js",    // Входной файл
-    output: "./dist/bundle.js", // Куда сохранять бандл
+    output: "./dist/bundle.js", // Выходной файл
     minify: true,               // Включить минификацию
+    minifyOptions: {
+        removeComments: true,   // Удалять комментарии
+        removeWhitespace: true, // Удалять лишние пробелы
+        minifyNames: true       // Сжимать имена переменных
+    },
     alias: {
       "@utils": "./src/utils"   // Алиас для удобства
     }
